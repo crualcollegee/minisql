ef00f1f1c74bdcedf64934d89fe6995befe37f21
cht <1051292989@qq.com>
2025-04-17 10:38:02 +0800

    bitmap_page.cpp accomplished

--- src/page/bitmap_page.cpp
+++ src/page/bitmap_page.cpp
@@ -7,14 +7,30 @@
  */
 template <size_t PageSize>
 bool BitmapPage<PageSize>::AllocatePage(uint32_t &page_offset) {
-    if (!IsPageFree(page_offset)){
+    if (next_free_page_>=GetMaxSupportedSize()){
         return false;
     }
-    uint32_t byte_index = page_offset / 8;
-    uint8_t bit_index = page_offset % 8;
-    bytes[byte_index]|=(1<<bit_index);
+    page_offset=next_free_page_;
+    uint32_t byte_index=page_offset / 8;
+    uint8_t bit_index=page_offset % 8;
     page_allocated_++;
-    return true;
+    bytes[byte_index]|=(1<<bit_index);
+    uint32_t count=0;
+
+    if (page_allocated_ == GetMaxSupportedSize()) {
+        next_free_page_ = GetMaxSupportedSize();
+        return true;
+    }
+
+    do{
+        count++;
+        next_free_page_+=1;
+        next_free_page_%=GetMaxSupportedSize();
+        if (IsPageFree(next_free_page_)){
+            return true;
+        }
+    }while(count!=GetMaxSupportedSize()+3);
+    return false;
 }
 
 /**
@@ -22,13 +38,20 @@ bool BitmapPage<PageSize>::AllocatePage(uint32_t &page_offset) {
  */
 template <size_t PageSize>
 bool BitmapPage<PageSize>::DeAllocatePage(uint32_t page_offset) {
+    if (page_offset>=GetMaxSupportedSize())
+    {
+        return false;
+    }
     if (IsPageFree(page_offset)){
         return false;
     }
-    uint32_t byte_index = page_offset / 8;
-    uint8_t bit_index = page_offset % 8;
-    bytes[byte_index]&=~(1<<bit_index);
     page_allocated_--;
+    uint32_t byte_index=page_offset / 8;
+    uint8_t bit_index=page_offset % 8;
+    bytes[byte_index]&=~(1<<bit_index);
+     if (page_offset < next_free_page_) {
+         next_free_page_ = page_offset;
+     }
     return true;
 }
 
@@ -37,14 +60,14 @@ bool BitmapPage<PageSize>::DeAllocatePage(uint32_t page_offset) {
  */
 template <size_t PageSize>
 bool BitmapPage<PageSize>::IsPageFree(uint32_t page_offset) const {
-    uint32_t byte_index = page_offset / 8;
-    uint8_t bit_index = page_offset % 8;
+    uint32_t byte_index=page_offset / 8;
+    uint8_t bit_index=page_offset % 8;
     return IsPageFreeLow(byte_index,bit_index);
 }
 
 template <size_t PageSize>
 bool BitmapPage<PageSize>::IsPageFreeLow(uint32_t byte_index, uint8_t bit_index) const {
-    return byte[byte_index]&(1<<bit_index)==0;
+    return (bytes[byte_index]&(1<<bit_index))==0;
 }
 
 template class BitmapPage<64>;
